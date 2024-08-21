5aeda924e04f8bcdc53c993dd38b35acd4d4fbf6
vick-ram <vickramodero6@gmail.com>
2024-08-21 07:57:58 -0700

    file-0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines a basic caching system without any limit """
+    
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
