e327fecf2042ba4ae040d66bebeb05f777b985d5
vick-ram <vickramodero6@gmail.com>
2024-08-28 03:57:09 -0700

    requirements

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,11 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """a simple route rendering a template"""
     return render_template('0-index.html')
 
 
-
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
