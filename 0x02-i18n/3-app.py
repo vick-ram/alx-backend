3dfc5e02235f5914d148fe6d11b37cfae54fd8a3
vick-ram <vickramodero6@gmail.com>
2024-08-30 07:48:09 -0700

    correction

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,18 +8,22 @@ app = Flask(__name__)
 
 babel = Babel(app)
 
+
 class Config:
     """Configuration class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
     """Determines the best matching locale from the request."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
@@ -27,6 +31,7 @@ def index():
     return render_template('3-index.html',
                            home_title=_('home_title'),
                            home_header=_('home_header'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
