b5835a6b5be426e31b7cd197cc7d9447512782b3
Vickram Odero Josephat <vickramodero6@gmail.com>
2024-09-25 11:31:45 +0300

    readme file

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,4 +7,4 @@ def index_range(page: int, page_size: int) -> tuple:
     index and an end index."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return start_index, end_index
